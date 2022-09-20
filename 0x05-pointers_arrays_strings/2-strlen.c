cd271baaf6cc449ca81adb938ab9afcc5039a2bd
longonaga <longjohnonaga@gmail.com>
2022-09-20 21:22:44 +0000

    modified

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,8 +5,8 @@
 *@s: string
 *Return: returns length as integer
 */
-int strlen(char *s)
+int _strlen(char *s)
 { int len = 0;
-while (*(s + len) != *\0 *)
+while (*(s + len) != *0*)
 len++;
 return (len); }
