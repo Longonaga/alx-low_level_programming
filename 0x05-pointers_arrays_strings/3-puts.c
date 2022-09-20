a135849b0593284d5ae907fb64981eb48fdd49a2
longonaga <longjohnonaga@gmail.com>
2022-09-20 21:40:28 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts - prints a string
+*@str: string to print
+*
+*Description prints a string
+*on success return no error
+*/
+
+void _puts(char *str)
+{
+         int i = 0;
+	 while(*(str + 1) != '\0')
+	   {
+	     putchar(*(str + 1));
+	     i++;
