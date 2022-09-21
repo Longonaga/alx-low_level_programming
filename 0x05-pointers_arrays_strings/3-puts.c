5a4cf2ccf1bea43454a9a4a70f981017ae3d48d1
longonaga <longjohnonaga@gmail.com>
2022-09-21 02:34:28 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
 *_puts - prints a string
@@ -12,7 +12,7 @@
 void _puts(char *str)
 {
          int i = 0;
-	 while(*(str + 1) != '\0')
+	 while(*(str + 1) != "\0")
 	   {
 	     putchar(*(str + 1));
 	     i++;
