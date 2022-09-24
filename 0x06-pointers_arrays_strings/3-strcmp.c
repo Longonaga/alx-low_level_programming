92ff09bc0379e13cf96e1a6597cef62a55434db4
onaga john <longjohnonaga@gmail.com>
2022-09-24 16:12:22 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,29 +16,37 @@ while (s1[a])
 {
 a++;
 }
+
 while (s2[b])
 {
 b++;
 }
+
 if (a <= b)
 {
 lim = a;
 }
+
 else
 {
 lim = b;
 }
+
 while (c <= lim)
 {
+
 if (s1[c] == s2[c])
 {
 c++;
 continue;
 }
+
 else
 {
 r = s1[c] - s2[c];
 break;
 }
 c++;
+}
+return (0);
 }
